abfcb752c8dfef9799d2b68b0b881bf9fd7ad640
Edmond Lai <edmondklai@gmail.com>
2017-07-27 14:37:54 -0500

    Update variable format

--- ej.py
+++ ej.py
@@ -1,35 +1,39 @@
 from censusAPI import my_Census
 from config import API_KEY
-import pandas as pd
+
 
 def main():
+    year = "2015"
+    survey = "acs5"
+    fields = ["NAME",
+              "GEOID",
+              "B17017_001E",
+              "B17017_002E",
+              "B02001_001E",
+              "B02001_003E",
+              "B02001_004E",
+              "B02001_005E",
+              "B02001_006E",
+              "B02001_007E",
+              "B02001_008E",
+              "B03003_001E",
+              "B03003_003E"]
+    geoFilter = ["for=block+group:*",
+                 "in=tract:*",
+                 "in=county:19",
+                 "in=state:17"]
+
+
     c = my_Census(API_KEY)
-    c.set_year("2015")
-    c.set_survey("acs5")
-    c.set_field(["NAME",
-                 "GEOID",
-                 "B17017_001E",
-                 "B17017_002E",
-                 "B02001_001E",
-                 "B02001_003E",
-                 "B02001_004E",
-                 "B02001_005E",
-                 "B02001_006E",
-                 "B02001_007E",
-                 "B02001_008E",
-                 "B03003_001E",
-                 "B03003_003E"
-                 ])
-
-    c.set_geoFilter(["for=block+group:*",
-                     "in=tract:*",
-                     "in=county:19",
-                     "in=state:17"])
-
+    c.set_year(year)
+    c.set_survey(survey)
+    c.set_field(fields)
+    c.set_geoFilter(geoFilter)
     c.contruct_query()
-    cesnus_frame = c.get()
-    print(a.head())
-    censsu_frame.to_csv("outFile", sep=',', header=True)
+    census_frame = c.get()
+
+    print(census_frame.head())
+    census_frame.to_csv("outFile.csv", sep=',', header=True)
 
 
 if __name__ == '__main__':
